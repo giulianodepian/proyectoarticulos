2d8855cebd77d510ebb0315ac5534404019f1650
De Pian, Giuliano <yulomax@gmail.com>
2021-04-08 13:48:16 -0300

    Creada y configurada base de datos inicial

--- articulos/models.py
+++ articulos/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+
+
+class Articulos(models.Model):
+    titulo = models.CharField(max_length=50)
+    fechaCreacion = models.DateTimeField(auto_now_add=True)
+    fechaModificacion = models.DateTimeField(auto_now=True)
+    descripcion = models.TextField()
+    autor = models.ForeignKey(User, on_delete=models.CASCADE)
